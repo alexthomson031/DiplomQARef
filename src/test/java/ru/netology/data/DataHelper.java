9130036caf1d2a5d5d1c7644e79b70d857f59d74
Netotest <athomson@mail.ru>
2025-01-23 13:13:41 +0300

    move kred's to b.g

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -22,73 +22,88 @@ public class DataHelper {
         String code;
     }
 
+    // Получение текущего месяца
     public static String getCurrentMonth() {
         LocalDate localDate = LocalDate.now();
         return String.format("%02d", localDate.getMonthValue());
     }
 
+    // Получение следующего года
     public static String getNextYear() {
         LocalDate localDate = LocalDate.now();
         return String.format("%ty", localDate.plusYears(1));
     }
 
+    // Генерация валидного имени
     public static String getValidName() {
         Faker faker = new Faker(new Locale("ru"));
         return faker.name().firstName() + " " + faker.name().lastName();
     }
 
+    // Генерация CVC с двумя цифрами
     public static String getCvcWithTwoDigits() {
         FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en"), new RandomService());
         return fakeValuesService.numerify("##");
     }
 
+    // Генерация CVC с одной цифрой
     public static String getCvcWithOneDigit() {
         FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en"), new RandomService());
         return fakeValuesService.numerify("#");
     }
 
+    // Генерация валидного CVC
     public static String getValidCvc() {
         FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en"), new RandomService());
         return fakeValuesService.numerify("###");
     }
 
+    // Генерация имени с одной буквой
     public static String getNameWithOneLetter() {
         return faker.lorem().characters(1);
     }
 
+    // Генерация имени с числами
     public static String getNameWithNumbers() {
         return faker.number().digits(7);
     }
 
+    // Генерация слишком длинного имени
     public static String getTooLongName() {
         return faker.lorem().fixedString(3000);
     }
 
+    // Генерация фамилии на латинице
     public static String getOnlySurnameInLatinLetters() {
         Faker faker = new Faker(new Locale("en"));
         return faker.name().lastName();
     }
 
+    // Генерация фамилии на русском
     public static String getOnlySurname() {
         Faker faker = new Faker(new Locale("ru"));
         return faker.name().lastName();
     }
 
+    // Генерация имени на латинице
     public static String getOnlyNameInLatinLetters() {
         Faker faker = new Faker(new Locale("en"));
         return faker.name().firstName();
     }
 
+    // Генерация имени на русском
     public static String getOnlyName() {
         Faker faker = new Faker(new Locale("ru"));
         return faker.name().firstName();
     }
 
+    // Генерация прошлого года
     public static String getLastYear() {
         LocalDate localDate = LocalDate.now();
         return String.format("%ty", localDate.minusYears(1));
     }
 
+    // Генерация прошлого месяца
     public static String getLastMonth() {
         LocalDate localDate = LocalDate.now();
         LocalDate lastMonth = localDate.minusMonths(1);
@@ -97,24 +112,26 @@ public class DataHelper {
         return monthValue;
     }
 
+    // Получение текущего года
     public static String getCurrentYear() {
         return String.format("%ty", Year.now());
     }
 
+    // Вынесенные данные для использования в коде
     public static String getApprovedNumber() {
-        return "4444 4444 4444 4441";
+        return System.getenv("APPROVED_CARD_NUMBER"); // Берем из переменной окружения
     }
 
     public static String getShortCardNumber() {
-        return "1111 2222 3333 444";
+        return System.getenv("SHORT_CARD_NUMBER"); // Берем из переменной окружения
     }
 
     public static String getInvalidCardNumber() {
-        return "9999 9999 9999 9999";
+        return System.getenv("INVALID_CARD_NUMBER"); // Берем из переменной окружения
     }
 
     public static String getDeclinedNumber() {
-        return "4444 4444 4444 4442";
+        return System.getenv("DECLINED_CARD_NUMBER"); // Берем из переменной окружения
     }
 
     public static String getValidNameInLatinLetters() {
